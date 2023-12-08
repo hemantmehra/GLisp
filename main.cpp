615a4e1d81e5489b602fce5dd02179b769953962
Hemant Mehra <mehra.hemant14@gmail.com>
2023-12-08 23:52:25 +0530

    add macro GObj

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 #include <GObject.h>
 #include <eval_apply.h>
 
-#define INTEGER LISP::Integer
-#define CONS LISP::Cons
+#define GObj LISP::GObject
 
 std::string LISP::Object::to_string()
 {
@@ -17,21 +16,23 @@ std::string LISP::Object::to_string()
 int main()
 {
     LISP::Interpreter interpreter;
-    LISP::GObject ob1(42), ob2(17);
     LISP::Environment env;
+    
+    GObj ob1(42), ob2(17);
+    
     std::string s("add"), s2("mul");
-    LISP::GObject sym_add(&s);
-    LISP::GObject sym_mul(&s2);
+    GObj sym_add(&s);
+    GObj sym_mul(&s2);
 
-    LISP::GObject ob3(&ob1, &ob2);
-    LISP::GObject ob4(LISP::GObject::Primitive_Proc::ADD);
-    LISP::GObject op_mul(LISP::GObject::Primitive_Proc::MUL);
+    GObj ob3(&ob1, &ob2);
+    GObj ob4(GObj::Primitive_Proc::ADD);
+    GObj op_mul(GObj::Primitive_Proc::MUL);
 
     env.set(&sym_add, &ob4);
     env.set(&sym_mul, &op_mul);
-    LISP::GObject ob5(&sym_mul, &ob3);
+    GObj ob5(&sym_mul, &ob3);
     assert(ob5.is_cons());
-    LISP::GObject* result = interpreter.eval(&ob5, &env);
+    GObj* result = interpreter.eval(&ob5, &env);
 
     std::cout << result->as_integer() << std::endl;
     
