567dc344c856f43139f2b39b330ca05968c1faff
Hemant Mehra <mehra.hemant14@gmail.com>
2023-12-18 17:45:27 +0530

    add macros

--- test.cpp
+++ test.cpp
@@ -3,6 +3,12 @@
 #include <Interpreter/Symbol.h>
 #include <Interpreter/Cons.h>
 
+#define OBJECT_CAST(x) std::static_pointer_cast<LISP::Object>(x)
+#define MAKE_SCALER(x) std::make_shared<LISP::Scaler>(x)
+#define MAKE_SYMBOL(x) std::make_shared<LISP::Symbol>(x)
+#define MAKE_CONS1(x) std::make_shared<LISP::Cons>(x)
+#define MAKE_CONS2(x, y) std::make_shared<LISP::Cons>(x, y)
+
 void test_scaler()
 {
     LISP::Scaler s1;
@@ -22,8 +28,8 @@ void test_symbol()
 
 void test_cons()
 {
-    std::shared_ptr<LISP::Object> s1 = std::static_pointer_cast<LISP::Object>(std::make_shared<LISP::Scaler>(42));
-    std::shared_ptr<LISP::Object> s2 = std::static_pointer_cast<LISP::Object>(std::make_shared<LISP::Scaler>(33));
+    std::shared_ptr<LISP::Object> s1 = OBJECT_CAST(MAKE_SCALER(42));
+    std::shared_ptr<LISP::Object> s2 = OBJECT_CAST(MAKE_SCALER(33));
 
     LISP::Cons c1(s1);
     TEST(c1.to_string() == "(42, nil)");
@@ -32,10 +38,23 @@ void test_cons()
     TEST(c2.to_string() == "(42, 33)");
 }
 
+void test_cons2()
+{
+    std::shared_ptr<LISP::Object> s1 = OBJECT_CAST(MAKE_SYMBOL("add"));
+    std::shared_ptr<LISP::Object> s2 = OBJECT_CAST(MAKE_SCALER(42));
+    std::shared_ptr<LISP::Object> s3 = OBJECT_CAST(MAKE_SCALER(33));
+
+    std::shared_ptr<LISP::Object> c2 = OBJECT_CAST(MAKE_CONS2(s2, s3));
+
+    LISP::Cons c3(s1, c2);
+    TEST(c3.to_string() == "(add, (42, 33))");
+}
+
 int main()
 {
     test_scaler();
     test_symbol();
     test_cons();
+    test_cons2();
     return 0;
 }
