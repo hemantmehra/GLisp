8bcf42285a37a352903d307e7315efb8c7904625
Hemant Mehra <mehra.hemant14@gmail.com>
2023-12-19 22:01:39 +0530

    define macros for shared_ptr

--- Interpreter/Interpreter.cpp
+++ Interpreter/Interpreter.cpp
@@ -3,8 +3,12 @@
 #include <Interpreter/Interpreter.h>
 #include <Interpreter/PrimitiveProcedure.h>
 
+#define CONS_PTR_CAST(x) std::dynamic_pointer_cast<Cons>(x)
+#define SCALER_PTR_CAST(x) std::dynamic_pointer_cast<Scaler>(x)
+#define OBJECT_PTR std::shared_ptr<Object>
+
 namespace LISP {
-    std::shared_ptr<Object> Interpreter::eval(std::shared_ptr<Object> obj, std::shared_ptr<Environment> env)
+    OBJECT_PTR Interpreter::eval(OBJECT_PTR obj, std::shared_ptr<Environment> env)
     {
         if (obj->is_scaler()) {
             return obj;
@@ -15,14 +19,14 @@ namespace LISP {
         }
 
         else {
-            std::shared_ptr<Cons> cons_obj = std::dynamic_pointer_cast<Cons>(obj);
-            std::shared_ptr<Object> proc = cons_obj->as_car();
-            std::shared_ptr<Object> args = cons_obj->as_cdr();
+            std::shared_ptr<Cons> cons_obj = CONS_PTR_CAST(obj);
+            OBJECT_PTR proc = cons_obj->as_car();
+            OBJECT_PTR args = cons_obj->as_cdr();
             return apply(eval(proc, env), args);
         }
     }
 
-    std::shared_ptr<Object> Interpreter::apply(std::shared_ptr<Object> proc, std::shared_ptr<Object> args)
+    OBJECT_PTR Interpreter::apply(OBJECT_PTR proc, OBJECT_PTR args)
     {
         (void) args;
 
@@ -33,22 +37,21 @@ namespace LISP {
         return std::make_shared<Scaler>();
     }
 
-    std::shared_ptr<Object> Interpreter::apply_dispatch(std::shared_ptr<Object> proc, std::shared_ptr<Object> args)
+    OBJECT_PTR Interpreter::apply_dispatch(OBJECT_PTR proc, OBJECT_PTR args)
     {
-        (void) args;
         std::shared_ptr<PrimitiveProcedure> proc_obj = std::dynamic_pointer_cast<PrimitiveProcedure>(proc);
 
         switch (proc_obj->type())
         {
         case PrimitiveProcedure::Type::Add:
         {
-            std::shared_ptr<Cons> cons_obj = std::dynamic_pointer_cast<Cons>(args);
-            std::shared_ptr<Object> arg1 = cons_obj->as_car();
-            std::shared_ptr<Object> arg2 = std::dynamic_pointer_cast<Cons>(cons_obj->as_cdr())->as_car();
+            std::shared_ptr<Cons> cons_obj = CONS_PTR_CAST(args);
+            OBJECT_PTR arg1 = cons_obj->as_car();
+            OBJECT_PTR arg2 = CONS_PTR_CAST(cons_obj->as_cdr())->as_car();
 
             if (arg1->is_scaler() && arg2->is_scaler()) {
-                std::shared_ptr<Scaler> arg1_s = std::dynamic_pointer_cast<Scaler>(arg1);
-                std::shared_ptr<Scaler> arg2_s = std::dynamic_pointer_cast<Scaler>(arg2);
+                std::shared_ptr<Scaler> arg1_s = SCALER_PTR_CAST(arg1);
+                std::shared_ptr<Scaler> arg2_s = SCALER_PTR_CAST(arg2);
                 return std::make_shared<Scaler>(arg1_s->as_integer() + arg2_s->as_integer());
             }
         }
